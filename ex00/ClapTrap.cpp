c5887af20fa18cf0021edf0c365803bb0a089717
Maja Kijewska <maja@MacBook-Air-Maja-2.local>
2026-01-11 17:28:09 +0100

    setter, getter, orthodox cannonical from

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -6,18 +6,82 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 16:56:44 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/10 16:59:06 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/11 02:13:54 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap()
-{
-	
+ClapTrap::ClapTrap() : _name(""), _hitPoint(10), _energyPoint(10), _demage(0) {
+    std::cout << "Default constructor called\n";
+};
+
+ClapTrap::ClapTrap(std::string name) {
+    _name = name;
+    std::cout << "Parameterized constructor called\n";
 }
 
-ClapTrap::~ClapTrap()
-{
-	
+ClapTrap::ClapTrap(const ClapTrap& obj) {
+    _name = obj._name;
+    std::cout << "Copy constructor called\n";
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &other) {
+    if (this != &other)
+    {
+        _name = other._name;
+        _hitPoint = other._hitPoint;
+        _energyPoint = other._energyPoint;
+        _demage = other._demage;
+    }
+    std::cout << "Assignment operator called\n";
+    return (*this);
+}
+
+ClapTrap::~ClapTrap() {
+    std::cout << "Destructor\n";
+}
+
+void ClapTrap::setName(std::string name) {
+    _name = name;
+}
+
+void ClapTrap::setHitPoint(int hitPoint) {
+    _hitPoint = hitPoint;
+}
+
+void ClapTrap::setEnergyPoint(int energyPoint) {
+    _energyPoint = energyPoint;
+}
+
+void ClapTrap::setDemage(int demage) {
+    _demage = demage;
+}
+
+std::string ClapTrap::getName( void ) const {
+    return (_name);
+}
+
+int ClapTrap::getHitPoint( void ) const {
+    return (_hitPoint);
+}
+
+int ClapTrap::getEnergyPoint( void ) const {
+    return (_energyPoint);
+}
+
+int ClapTrap::getDemage( void ) const {
+    return (_demage);
+}
+
+void ClapTrap::attack(const std::string &target) {
+    
+}
+
+void ClapTrap::takeDemage(unsigned int amount) {
+
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+    
 }
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 16:57:47 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/10 20:18:25 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/11 02:06:34 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,21 @@ class ClapTrap
 		int _energyPoint;
 		int _demage;
 	public:
-		ClapTrap() : _name(""), _hitPoint(10), _energyPoint(10), _demage(0) {};
+		ClapTrap();
 		ClapTrap(std::string name);
 		ClapTrap(const ClapTrap& obj);
 		ClapTrap &operator=(const ClapTrap &other);
 		~ClapTrap();
+
+		void setName(std::string name);
+		void setHitPoint(int hitPoint);
+		void setEnergyPoint(int energyPoint);
+		void setDemage(int demage);
+		std::string getName( void ) const ;
+		int getHitPoint( void ) const ;
+		int getEnergyPoint( void ) const ;
+		int getDemage( void ) const ;
+
 		void attack(const std::string &target);
 		void takeDemage(unsigned int amount);
 		void beRepaired(unsigned int amount);
